fb258503c349148d7b912bda4b980a428c8b330b
Jeff Coleman <progressions@gmail.com>
2008-11-09 11:31:38 -0600

    Installed rspec

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,6 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
+require 'mocha'
 require 'test_help'
 require 'test/rails'
 require 'authenticated_test_helper'
