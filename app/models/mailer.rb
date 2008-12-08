2deceeaf4dc4cde3b3eed1b27115fdcdb168419b
Jeff Coleman <jeffcoleman@jeff.local>
2008-12-08 17:51:44 -0600

    More updates to mail

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -1,7 +1,7 @@
 class Mailer < ActionMailer::Base  
   def signup_confirmation fan
     recipients  fan.email
-    from        global_settings.email
+    from        "#{global_settings.artist_name} <#{global_settings.email}>"
     subject     "[#{global_settings.mail_tag}] Thank you for joining the #{global_settings.artist_name} mailing list!"
     body        :fan => fan, :global_settings => global_settings
     content_type "text/html" 
@@ -9,7 +9,7 @@ class Mailer < ActionMailer::Base
   
   def signup_report fan
     recipients  global_settings.admin_email
-    from        global_settings.email
+    from        "#{global_settings.artist_name} <#{global_settings.email}>"
     subject     "[New fan] #{fan.email} has joined the mailing list!"
     body        :fan => fan, :global_settings => global_settings
     content_type "text/html"
@@ -17,7 +17,7 @@ class Mailer < ActionMailer::Base
   
   def unsubscribe_report fan
     recipients  global_settings.admin_email
-    from        global_settings.email
+    from        "#{global_settings.artist_name} <#{global_settings.email}>"
     subject     "[Unsubscribed] #{fan.email} has unsubscribed from the mailing list!"
     body        :fan => fan, :global_settings => global_settings
     content_type "text/html"
@@ -25,7 +25,7 @@ class Mailer < ActionMailer::Base
   
   def comment_notification user, blog, comment
     recipients  user.email
-    from        global_settings.email
+    from        "#{global_settings.artist_name} <#{global_settings.email}>"
     subject     "[New comment] #{comment.name} has commented on your blog posting '#{blog.title}'"
     body        :user => user, :blog => blog, :comment => comment, :global_settings => global_settings
     content_type "text/html"
@@ -33,7 +33,7 @@ class Mailer < ActionMailer::Base
   
   def comment_subscription email, blog, comment
     recipients  email
-    from        global_settings.email
+    from        "#{global_settings.artist_name} <#{global_settings.email}>"
     subject     "[New comment] #{comment.name} has commented on the blog posting '#{blog.title}'"
     body        :email => email, :blog => blog, :comment => comment, :global_settings => global_settings
     content_type "text/html"
