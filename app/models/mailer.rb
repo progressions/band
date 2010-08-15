338a540803018367c9bda43bf8324e45c262fc91
Isaac Priestley <progressions@gmail.com>
2010-08-15 13:55:47 -0500

    use email layout

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -1,9 +1,11 @@
 class Mailer < ActionMailer::Base  
+  layout 'email_layout'
+  
   def signup_confirmation fan
     recipients  fan.email
     from        "#{global_settings.artist_name} <#{global_settings.email}>"
     subject     "[#{global_settings.mail_tag}] Thank you for joining the #{global_settings.artist_name} mailing list!"
-    body        :fan => fan, :global_settings => global_settings
+    body        :fan => fan, :global_settings => global_settings, :title => "Thank you for joining the #{global_settings.artist_name} mailing list!"
     content_type "text/html" 
   end
   
@@ -11,7 +13,7 @@ class Mailer < ActionMailer::Base
     recipients  global_settings.admin_email
     from        "#{global_settings.artist_name} <#{global_settings.email}>"
     subject     "[New fan] #{fan.email} has joined the mailing list!"
-    body        :fan => fan, :global_settings => global_settings
+    body        :fan => fan, :global_settings => global_settings, :title => "#{fan.email} has joined the mailing list!"
     content_type "text/html"
   end
   
@@ -19,7 +21,7 @@ class Mailer < ActionMailer::Base
     recipients  global_settings.admin_email
     from        "#{global_settings.artist_name} <#{global_settings.email}>"
     subject     "[Unsubscribed] #{fan.email} has unsubscribed from the mailing list!"
-    body        :fan => fan, :global_settings => global_settings
+    body        :fan => fan, :global_settings => global_settings, :title => "#{fan.email} has unsubscribed from the mailing list!"
     content_type "text/html"
   end
   
@@ -27,7 +29,7 @@ class Mailer < ActionMailer::Base
     recipients  user.email
     from        "#{global_settings.artist_name} <#{global_settings.email}>"
     subject     "[New comment] #{comment.name} has commented on your blog posting '#{blog.title}'"
-    body        :user => user, :blog => blog, :comment => comment, :global_settings => global_settings
+    body        :user => user, :blog => blog, :comment => comment, :global_settings => global_settings, :title => "#{comment.name} has commented on your blog posting '#{blog.title}'"
     content_type "text/html"
   end
   
@@ -35,7 +37,7 @@ class Mailer < ActionMailer::Base
     recipients  email
     from        "#{global_settings.artist_name} <#{global_settings.email}>"
     subject     "[New comment] #{comment.name} has commented on the blog posting '#{blog.title}'"
-    body        :email => email, :blog => blog, :comment => comment, :global_settings => global_settings
+    body        :email => email, :blog => blog, :comment => comment, :global_settings => global_settings, :title => "#{comment.name} has commented on the blog posting '#{blog.title}'"
     content_type "text/html"
   end
     
@@ -43,7 +45,7 @@ class Mailer < ActionMailer::Base
     recipients  fan.email
     from        "#{global_settings.artist_name} <#{global_settings.email}>"
     subject     "[#{global_settings.mail_tag}] #{mail.title}"
-    body        :fan => fan, :mail => mail, :global_settings => global_settings, :shows => Show.upcoming, :blogs => Blog.active.posted_yet.find(:all, :limit => 3)
+    body        :fan => fan, :mail => mail, :global_settings => global_settings, :shows => Show.upcoming, :blogs => Blog.active.posted_yet.find(:all, :limit => 3), :title => mail.title
     content_type "text/html"
   end
   
@@ -51,7 +53,7 @@ class Mailer < ActionMailer::Base
     recipients  status_presenter.site.admin_email
     from        "#{status_presenter.site.artist_name} <#{status_presenter.site.email}>"
     subject     "[#{mail_tag}] Status Report for #{status_presenter.to_date}"
-    body        :status_presenter => status_presenter
+    body        :status_presenter => status_presenter, :title => "Status Report for #{status_presenter.to_date}"
     content_type  "text/html"
   end
   
