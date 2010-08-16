ce8fec45ae65903afeee6eee1e36b7d15488af77
Isaac Priestley <progressions@gmail.com>
2010-08-16 17:48:00 -0500

    provide sidebar content for all emails

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -5,7 +5,11 @@ class Mailer < ActionMailer::Base
     recipients  fan.email
     from        "#{global_settings.artist_name} <#{global_settings.email}>"
     subject     "[#{global_settings.mail_tag}] Thank you for joining the #{global_settings.artist_name} mailing list!"
-    body        :fan => fan, :global_settings => global_settings, :title => "Thank you for joining the #{global_settings.artist_name} mailing list!"
+    body        :fan => fan, 
+                :global_settings => global_settings, 
+                :title => "Thank you for joining the #{global_settings.artist_name} mailing list!",
+                :shows => Show.find(:all, :limit => 3, :order => 'date ASC', :conditions => ["date > ?", Time.now]), 
+                :blogs => Blog.active.posted_yet.find(:all, :limit => 3, :order => "posted_at DESC, created_at DESC")
     content_type "text/html" 
   end
   
@@ -13,7 +17,11 @@ class Mailer < ActionMailer::Base
     recipients  global_settings.admin_email
     from        "#{global_settings.artist_name} <#{global_settings.email}>"
     subject     "[New fan] #{fan.email} has joined the mailing list!"
-    body        :fan => fan, :global_settings => global_settings, :title => "#{fan.email} has joined the mailing list!"
+    body        :fan => fan, 
+                :global_settings => global_settings, 
+                :title => "#{fan.email} has joined the mailing list!",
+                :shows => Show.find(:all, :limit => 3, :order => 'date ASC', :conditions => ["date > ?", Time.now]), 
+                :blogs => Blog.active.posted_yet.find(:all, :limit => 3, :order => "posted_at DESC, created_at DESC")
     content_type "text/html"
   end
   
@@ -21,15 +29,25 @@ class Mailer < ActionMailer::Base
     recipients  global_settings.admin_email
     from        "#{global_settings.artist_name} <#{global_settings.email}>"
     subject     "[Unsubscribed] #{fan.email} has unsubscribed from the mailing list!"
-    body        :fan => fan, :global_settings => global_settings, :title => "#{fan.email} has unsubscribed from the mailing list!"
-    content_type "text/html"
+    body        :fan => fan, 
+                :global_settings => global_settings, 
+                :title => "#{fan.email} has unsubscribed from the mailing list!"
+    content_type "text/html",
+                :shows => Show.find(:all, :limit => 3, :order => 'date ASC', :conditions => ["date > ?", Time.now]), 
+                :blogs => Blog.active.posted_yet.find(:all, :limit => 3, :order => "posted_at DESC, created_at DESC")
   end
   
   def comment_notification user, blog, comment
     recipients  user.email
     from        "#{global_settings.artist_name} <#{global_settings.email}>"
     subject     "[New comment] #{comment.name} has commented on your blog posting '#{blog.title}'"
-    body        :user => user, :blog => blog, :comment => comment, :global_settings => global_settings, :title => "#{comment.name} has commented on your blog posting '#{blog.title}'"
+    body        :user => user, 
+                :blog => blog, 
+                :comment => comment, 
+                :global_settings => global_settings, 
+                :title => "#{comment.name} has commented on your blog posting '#{blog.title}'",
+                :shows => Show.find(:all, :limit => 3, :order => 'date ASC', :conditions => ["date > ?", Time.now]), 
+                :blogs => Blog.active.posted_yet.find(:all, :limit => 3, :order => "posted_at DESC, created_at DESC")
     content_type "text/html"
   end
   
@@ -37,15 +55,26 @@ class Mailer < ActionMailer::Base
     recipients  email
     from        "#{global_settings.artist_name} <#{global_settings.email}>"
     subject     "[New comment] #{comment.name} has commented on the blog posting '#{blog.title}'"
-    body        :email => email, :blog => blog, :comment => comment, :global_settings => global_settings, :title => "#{comment.name} has commented on the blog posting '#{blog.title}'"
-    content_type "text/html"
+    body        :email => email, 
+                :blog => blog, 
+                :comment => comment, 
+                :global_settings => global_settings, 
+                :title => "#{comment.name} has commented on the blog posting '#{blog.title}'"
+    content_type "text/html",
+                :shows => Show.find(:all, :limit => 3, :order => 'date ASC', :conditions => ["date > ?", Time.now]), 
+                :blogs => Blog.active.posted_yet.find(:all, :limit => 3, :order => "posted_at DESC, created_at DESC")
   end
     
   def mail fan, mail
     recipients  fan.email
     from        "#{global_settings.artist_name} <#{global_settings.email}>"
     subject     "[#{global_settings.mail_tag}] #{mail.title}"
-    body        :fan => fan, :mail => mail, :global_settings => global_settings, :shows => Show.find(:all, :limit => 3, :order => 'date ASC', :conditions => ["date > ?", Time.now]), :blogs => Blog.active.posted_yet.find(:all, :limit => 3, :order => "posted_at DESC, created_at DESC"), :title => mail.title
+    body        :fan => fan, 
+                :mail => mail, 
+                :global_settings => global_settings, 
+                :shows => Show.find(:all, :limit => 3, :order => 'date ASC', :conditions => ["date > ?", Time.now]), 
+                :blogs => Blog.active.posted_yet.find(:all, :limit => 3, :order => "posted_at DESC, created_at DESC"), 
+                :title => mail.title
     content_type "text/html"
   end
   
@@ -53,7 +82,10 @@ class Mailer < ActionMailer::Base
     recipients  status_presenter.site.admin_email
     from        "#{status_presenter.site.artist_name} <#{status_presenter.site.email}>"
     subject     "[#{mail_tag}] Status Report for #{status_presenter.to_date}"
-    body        :status_presenter => status_presenter, :title => "Status Report for #{status_presenter.to_date}"
+    body        :status_presenter => status_presenter, :title => "Status Report for #{status_presenter.to_date}",
+                :global_settings => global_settings, 
+                :shows => Show.find(:all, :limit => 3, :order => 'date ASC', :conditions => ["date > ?", Time.now]), 
+                :blogs => Blog.active.posted_yet.find(:all, :limit => 3, :order => "posted_at DESC, created_at DESC")
     content_type  "text/html"
   end
   
