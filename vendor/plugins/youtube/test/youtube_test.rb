e2e3d1c6f9e8e15a06fabcdc5f2d8863b23a47b8
Isaac Priestley <progressions@gmail.com>
2012-02-25 21:12:29 -0600

    more

--- vendor/plugins/youtube/test/youtube_test.rb
+++
@@ -1,9 +0,0 @@
-require File.dirname(__FILE__) + '/test/test_helper'
-require 'test/unit'
-
-class YoutubeTest < Test::Rails::TestCase
-  # Replace this with your real tests.
-  def test_this_plugin
-    flunk
-  end
-end
